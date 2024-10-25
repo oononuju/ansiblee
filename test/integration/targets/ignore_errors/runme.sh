94a2502be94026786e74d9bfbb35b735651dd46e
s-hertel <19572925+s-hertel@users.noreply.github.com>
2024-10-25 13:29:32 -0400

    typo

--- test/integration/targets/ignore_errors/runme.sh
+++ test/integration/targets/ignore_errors/runme.sh
@@ -4,7 +4,7 @@ set -eux
 ansible-playbook -i ../../inventory test_ignore_errors.yml "$@"
 
 if ansible-playbook -i ../../inventory test_ignore_errors_false.yml "$@" > out.txt; then
-    echo 'Playbook expected to fail succeded'
+    echo 'Playbook expected to fail succeeded'
     exit 1
 fi
 # The first task should fail and not be ignored

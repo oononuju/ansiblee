bfe28c892885e6d8d09570bfd0a513416f98b0ae
Abhijeet Kasurde <akasurde@redhat.com>
2024-10-30 12:36:05 -0700

    test: Update regex for required and default in FieldAttributes
    Fixes: #61460
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- test/sanity/code-smell/required-and-default-attributes.py
+++ test/sanity/code-smell/required-and-default-attributes.py
@@ -8,7 +8,7 @@ def main():
     for path in sys.argv[1:] or sys.stdin.read().splitlines():
         with open(path, 'r') as path_fd:
             for line, text in enumerate(path_fd.readlines()):
-                match = re.search(r'(FieldAttribute.*(default|required).*(default|required))', text)
+                match = re.search(r'(FieldAttribute.*(default|required)=.*(default|required)=)', text)
 
                 if match:
                     print('%s:%d:%d: use only one of `default` or `required` with `FieldAttribute`' % (
